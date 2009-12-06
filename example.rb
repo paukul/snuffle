153b6ba81631258193cb20496105427155247bf5
Pascal Friederich <paukul@gmail.com>
2009-12-06 23:50:16 +0100

    Read cookie from cookie file. I'll need that for later ;)

--- example.rb
+++ example.rb
@@ -5,7 +5,9 @@ require 'bertrpc'
 class RabbitRPC
   attr_reader :host, :nodename
 
-  def initialize(host, nodename, vhost = '/')
+  def initialize(host, nodename, cookie = nil, vhost = '/')
+    @cookie = cookie || read_cookie
+    puts @cookie
     @vhost = vhost
     @node = "#{nodename}@#{host}".to_sym
     @svc = BERTRPC::Service.new(host, 8821)
@@ -22,14 +24,24 @@ class RabbitRPC
   def list_bindings
     @svc.call.rabbitrpc.list_bindings(@node, @vhost)
   end
+  
+  private
+  def read_cookie
+    cookie_file = File.expand_path("~/.erlang.cookie")
+    if File.exist?(cookie_file)
+      File.read(cookie_file)
+    else
+      raise "No cookie set and cookie file not found"
+    end
+  end
 end
 
 rabbit = RabbitRPC.new(`hostname`.chomp, "rabbit")
-puts "Listing Queues:"
-rabbit.list_queues.each { |queue| puts queue.inspect }
-puts
-puts "Listing Exchanges:"
-rabbit.list_exchanges.each { |exchange| puts exchange.inspect }
-puts
-puts "Listing Bindings"
-rabbit.list_bindings.each { |binding| puts binding.inspect }
+# puts "Listing Queues:"
+# rabbit.list_queues.each { |queue| puts queue.inspect }
+# puts
+# puts "Listing Exchanges:"
+# rabbit.list_exchanges.each { |exchange| puts exchange.inspect }
+# puts
+# puts "Listing Bindings"
+# rabbit.list_bindings.each { |binding| puts binding.inspect }
