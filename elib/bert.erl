e242c14fe42a650c6163e1d015c69c1164681cf1
Pascal Friederich <paukul@gmail.com>
2009-12-06 21:57:17 +0100

    handled true/false cases in BERT encoding not standard conform

--- elib/bert.erl
+++ elib/bert.erl
@@ -5,5 +5,7 @@ encode(Data) ->
   case Data of
     [{_A, _B}|_Others] -> {bert, dict, Data};
     [] -> {bert, nil};
+    true -> {bert, true};
+    false -> {bert, false};
     _Any -> _Any
   end.
