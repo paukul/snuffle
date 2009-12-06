1ef67f3aa784eb71977b5d43bca31cdc127929b3
Pascal Friederich <paukul@gmail.com>
2009-12-06 22:44:06 +0100

    detect dicts correctly

--- elib/bert.erl
+++ elib/bert.erl
@@ -3,9 +3,19 @@
 
 encode(Data) ->
   case Data of
-    [{_A, _B}|_Others] -> {bert, dict, Data};
+    [{_A, _B}|_Others] -> check_for_dict(Data);
     [] -> {bert, nil};
     true -> {bert, true};
     false -> {bert, false};
     _Any -> _Any
   end.
+
+check_for_dict(List) ->
+  check_for_dict(List, List).
+
+check_for_dict([{_A, _B}|Tail], List) ->
+  check_for_dict(Tail, List);
+check_for_dict([], List) ->
+  {bert, dict, List};
+check_for_dict(_Any, List) ->
+  {bert, List}.
